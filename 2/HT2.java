d5c9c5217df5caa19eed3e9ec8be105148a1e2af
AlermGB <116220194+AlermGB@users.noreply.github.com>
2023-07-21 13:04:29 +0300

    Update HT2.java

--- 2/HT2.java
+++ 2/HT2.java
@@ -1,1 +1,13 @@
-
+public class HT2 {
+    public static void main(String[] args) {
+        try {
+            int d = 0;
+            //double catchedRes1 = intArray[8] / d; самое простое - избавиться от неинициализарованного массива. но код странный, т.к. d = 0 задается в блоке try, catch всегда ловит эксепшн
+            double catchedRes1 = 8/d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+    }
+}
